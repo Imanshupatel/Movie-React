bdd414ecb7d49af5bf0234926df9bfa08c9ccb62
Imanshupatel <himanshulakhankiya2003@gmail.com>
2025-06-17 12:14:17 +0530

    Update Header.jsx

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -4,18 +4,6 @@ export const Header = () => {
     return (
         <>
             <header className="section-navbar">
-                <section className="top_txt">
-                    <div className="head container">
-                        <div className="head_txt">
-                            <p>I am Best Dealer of Gold and Diamond</p>
-                        </div>
-                        <div className="sing_in_up">
-                            <NavLink to="#">SIGN IN</NavLink>
-                            <NavLink to="#">SIGN UP</NavLink> 
-                        </div>
-                    </div>
-                </section>
-
                 <div className="container">
                     <div className="navbar-brand">
                         <NavLink to="index">
